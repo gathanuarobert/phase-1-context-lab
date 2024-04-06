d05a1cf7d4b5462a97bb939dc4c1d4521238415e
Robert Gathanua <robertgathanua@gamail.com>
2024-04-06 23:09:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,79 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeData) {
+    return {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(employeeDataArray) {
+    return employeeDataArray.map(employeeData => createEmployeeRecord(employeeData));
+}
+
+function createTimeInEvent(employeeRecord, timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+
+    employeeRecord.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    });
+
+    return createEmployeeRecords;
+}
+
+function createTimeOutEvent(employeeRecord, timeStamp) {
+    const [date, hour] = timeStamp.split(' ');
+
+    employeeRecord.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    });
+
+    return createEmployeeRecordsmployeeRecord;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+    const timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+
+    if (!timeInEvent || !timeOutEvent) {
+        return 0; // Return 0 if either timeInEvent or timeOutEvent is not found for the given date
+    }
+
+    const timeInHour = timeInEvent.hour / 100;
+    const timeOutHour = timeOutEvent.hour / 100;
+
+    return timeOutHour - timeInHour;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+    const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    const payRate = employeeRecord.payPerHour;
+    
+    return hoursWorked * payRate;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employeeRecord => employeeRecord.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecordsArray) {
+    let totalPayroll = 0;
+
+    // Iterate over each employee record and accumulate their total wages
+    employeeRecordsArray.forEach(employeeRecord => {
+        const wagesForEmployee = allWagesFor(employeeRecord);
+        totalPayroll += wagesForEmployee;
+    });
+
+    return totalPayroll;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
